72008f2f1d5d46039a9bb9ea01ee92235eed24d1
rehellinen <rehellinen@foxmail.com>
2019-12-05 00:19:49 +0800

    refactor: add Config class

---
+++ src/class/Config.js
@@ -0,0 +1,40 @@
+/**
+ *  Index.js
+ *  Create By rehellinen
+ *  Create On 2019/3/19 22:00
+ */
+import { isProduction, rRoot } from '../utils/utils'
+
+export class Config {
+  // 配置
+  config = {}
+
+  /**
+   * 读取配置文件
+   * @param dir 相对路径，相对于process.cwd()
+   * @private
+   */
+  loadConfig (dir = 'config') {
+    const baseConf = require(rRoot(`${dir}/base.conf`))['default']
+    const devConf = require(rRoot(`${dir}/dev.conf`))['default']
+    const prodConf = require(rRoot(`${dir}/prod.conf`))['default']
+    this.config = {
+      ...baseConf,
+      ...(isProduction ? prodConf : devConf)
+    }
+  }
+
+  // 获取配置
+  getConfig (prefix = '') {
+    let res = this.config
+    prefix.split('.').forEach(item => {
+      if (!item) {
+        return
+      }
+      res = res[item.toUpperCase()]
+    })
+    return res
+  }
+}
+
+export const config = new Config()
