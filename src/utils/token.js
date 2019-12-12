56ef80edef0687cf052eb230cbfcedfca6447e3a
rehellinen <rehellinen@foxmail.com>
2019-12-12 22:33:19 +0800

    feat: add more utils to help use token

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,6 +1,7 @@
 import { Token } from '../class'
 import { TokenType } from './enum'
-import { InvalidToken } from '../exception'
+import {InvalidToken, NoAuthority, NotFound} from '../exception'
+import {UserModel} from "../model/UserModel"
 
 /**
  * 获取access_token和refresh_token
@@ -31,6 +32,10 @@ export const getRefreshToken = (id, payload) => {
   return Token.getInstance().getRefreshToken(id, payload)
 }
 
+export const verifyToken = (token) => {
+  return Token.getInstance().verify()
+}
+
 export const verifyAccessToken = (token) => {
   const decoded = Token.getInstance().verify()
   if (decoded.type !== TokenType.ACCESS) {
@@ -45,3 +50,23 @@ export const verifyRefreshToken = (token) => {
   }
 }
 
+export const parseHeader = async (ctx, type = TokenType.ACCESS) => {
+  if (!ctx.header || !ctx.header.authorization) {
+    throw new NoAuthority({ message: '没有携带令牌' })
+  }
+  const res = ctx.header.authorization.split(' ')
+  const [prefix, token] = res
+  if (res.length !== 2 || prefix.toLowerCase() !== 'bearer') {
+    throw new NoAuthority()
+  }
+
+  const payload = verifyToken(token)
+  if (payload.type !== type) {
+    throw new NoAuthority({ message: '令牌类型错误' })
+  }
+  const user = await new UserModel().getOneById({ id: payload.id })
+  if (!user) {
+    throw new NotFound({ message: '用户不存在' })
+  }
+  ctx.userInfo = user
+}
