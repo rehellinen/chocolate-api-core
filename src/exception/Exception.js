455d57bb35fe6d5780666eed3be98ca1dc5f383c
rehellinen <rehellinen@foxmail.com>
2019-12-12 20:28:22 +0800

    style: reduce duplicate code

--- src/exception/Exception.js
+++ src/exception/Exception.js
@@ -1,24 +1,21 @@
 export class Exception extends Error {
   constructor (config = {}) {
     super()
-    if (Reflect.has(config, 'httpCode')) {
-      this.httpCode = config.httpCode
-    }
-    if (Reflect.has(config, 'status')) {
-      this.status = config.status
-    }
-    if (Reflect.has(config, 'message')) {
-      this.message = config.message
-    }
-    if (Reflect.has(config, 'data')) {
-      this.data = config.data
-    }
+    const fields = ['httpCode', 'status', 'message', 'data']
+    fields.forEach(item => {
+      if (Reflect.has(config, item)) {
+        this[item] = config[item]
+      }
+    })
   }
 
-  setDefault ({ httpCode, status, message }) {
-    if (!this.httpCode) this.httpCode = httpCode
-    if (!this.status) this.status = status
-    if (!this.message) this.message = message
+  setDefault (config = {}) {
+    const fields = ['httpCode', 'status', 'message']
+    fields.forEach(item => {
+      if (Reflect.has(config, item) && !Reflect.has(this, item)) {
+        this[item] = config[item]
+      }
+    })
   }
 
   getError () {
