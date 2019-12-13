8e2c005c478f933229ae2bcfdc7f5b7d37bee3a0
rehellinen <rehellinen@foxmail.com>
2019-12-13 12:55:47 +0800

    refactor: export model files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export * from './class'
 export * from './decorator'
 export * from './exception'
 export * from './middleware'
+export * from './model'
 export * from './utils'
 
 export default Server
