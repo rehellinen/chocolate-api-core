bd29c821cef3f9becde56e1d2251ccfdc7f5c6e2
rehellinen <rehellinen@foxmail.com>
2020-01-31 20:07:18 +0800

    feat: add methods to user model

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -1,9 +1,20 @@
 import { Model } from '../class'
+import { NotFound } from '../exception'
 
 export class UserModel extends Model {
   constructor () {
     super({
       tableName: 'user'
     })
+  }
+
+  getUserByAccount (account) {
+    const user = this.getOne({
+      condition: { account }
+    })
+    if (!user) {
+      throw new NotFound({ message: '该账号不存在' })
+    }
+    return user
   }
 }
