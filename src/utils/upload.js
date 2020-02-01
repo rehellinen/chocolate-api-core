bf1fb2fa353599bea1aa9f69c2aacd243215f6b2
rehellinen <rehellinen@foxmail.com>
2020-02-01 15:18:44 +0800

    fix: fix bug that can't get config

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -4,24 +4,24 @@
  *  Create On 2019/3/12 12:54
  */
 import Multer from 'koa-multer'
-import { dirExists, getTodayDate, rRoot } from '../utils'
+import { dirExists, getTodayDate, rRoot, generateSalt } from '../utils'
 import { config } from '../class'
 
 export const upload = () => async (ctx, next) => {
   // 获取当前年月日组合而成的字符串
   const today = getTodayDate()
   // 判断是否有存放文件的文件夹
-  const destination = rRoot(`${config.get('UPLOAD_DIR')}/${today}`)
+  const destination = rRoot(`${config.get('UPLOAD.UPLOAD_DIR')}/${today}`)
   await dirExists(destination)
   // koa-multer中间件
   const storage = Multer.diskStorage({
     destination,
     filename (ctx, file, cb) {
       const ext = file.originalname.split('.').pop()
-      const filename = `${Date.now()}.${ext}`
+      const filename = Date.now() + generateSalt(5) + '.' + ext
       cb(null, filename)
     }
   })
   await Multer({ storage })
-    .single(config.get('UPLOAD_NAME'))(ctx, next)
+    .single(config.get('UPLOAD.UPLOAD_NAME'))(ctx, next)
 }
