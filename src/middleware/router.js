4202f4611ece9662a3c97b34ca1b5a1f99810539
rehellinen <rehellinen@foxmail.com>
2020-02-12 20:55:52 +0800

    refactor: edit middleware for new router layer

--- src/middleware/router.js
+++ src/middleware/router.js
@@ -4,23 +4,29 @@
  *  Create On 2018/10/25 20:37
  */
 import Router from 'koa-router'
-import glob from 'glob'
-import R from 'ramda'
 import { rRoot } from '../utils'
-import { routerMap } from '../decorator'
+import { routerMap } from '../router'
 import { config } from '../class'
 
-export const router = app => {
+export default app => {
   const router = new Router()
   // 执行路由文件
-  glob
-    .sync(rRoot(`${config.get('dir.router')}/**/*.js`))
-    .forEach(item => require(item))
+  const routerPath = rRoot(config.get('dir.module').app, 'router.js')
+  require(routerPath)
+
   // 生成路由
-  for (const conf of routerMap.values()) {
-    const routerPath = conf.target.prefix + conf.path
-    router[conf.method](routerPath, ...conf.actions)
+  for (const ctorConf of routerMap.values()) {
+    Object.values(ctorConf).forEach(routerConf => {
+      const { method, url, action } = routerConf
+
+      router[method](url, action)
+    })
   }
+
+  // router.use((ctx) => {
+  //   console.log(123)
+  // })
+
   app.use(router.routes())
   app.use(router.allowedMethods())
 }
