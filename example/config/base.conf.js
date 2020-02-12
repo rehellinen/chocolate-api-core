a7e0a0e77a65b15a16a49d66cd9dd22d2e71d51c
rehellinen <rehellinen@foxmail.com>
2020-02-12 13:10:04 +0800

    refactor: use new config for path resolving

--- example/config/base.conf.js
+++ example/config/base.conf.js
@@ -1,24 +1,20 @@
-/**
- *  base.conf.js
- *  Create By rehellinen
- *  Create On 2018/10/26 15:07
- */
 // 框架核心功能配置
 export default {
   PORT: 3000,
   HOST: '127.0.0.1',
   DEBUG: true,
   MIDDLEWARE: [],
+  // 配置文件存放的路径
   DIR: {
-    CONTROLLER: 'example/app/controller',
-    MODEL: 'example/app/model',
-    ROUTER: 'example/app/router',
+    MODULE: {
+      app: 'example/app'
+    },
     EXCEPTION: 'example/common/exception',
     MIDDLEWARE: 'example/common/middleware',
     VALIDATE: 'example/common/validate'
   },
   CORS: {
-    OPEN: false,
+    OPEN: true,
     METHODS: ['GET', 'POST', 'DELETE', 'PUT'],
     ORIGIN: '*', // Access-Control-Allow-Origin
     MAX_AGE: 5, // Access-Control-Max-Age
@@ -28,18 +24,15 @@ export default {
     // Access-Control-Allow-Methods
     ALLOW_HEADERS: ['Content-Type', 'Authorization', 'Accept', 'token', 'Content-Length', 'X-Requested-With']
   },
-  // 上传文件相关
   UPLOAD: {
     UPLOAD_DIR: 'upload', // 上传文件的根目录，upload表示/upload/
     UPLOAD_NAME: 'file' // 表单中表示上传文件的key
   },
-  // TOKEN相关
   TOKEN: {
     SECRET: 'github.com/rehellinen', // 生成token的前缀
     ACCESS_EXPIRES_IN: 7200, // access_token过期时间（单位为s）
     REFRESH_EXPIRES_IN: 3600 * 24 * 30 // refresh_token过期时间
   },
-  // 模型中用到的配置
   MODEL: {
     CONVERT_FIELDS: true, // 是否自动转换驼峰命名法 / 下划线命名法
     PAGE_SIZE: 15 // 分页大小
