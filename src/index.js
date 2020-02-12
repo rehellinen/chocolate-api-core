e57fc827eb05996514f9e7375b2d2ef2fd193268
rehellinen <rehellinen@foxmail.com>
2020-02-12 22:04:07 +0800

    improvement: update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { Server } from './class'
 export * from './class'
 export * from './decorator'
 export * from './exception'
-export * from './middleware'
 export * from './model'
+export * from './router'
 export * from './utils'
 
 export default Server
