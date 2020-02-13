d935e1cf80b4edf17373f5fd66e1b43c1145bc66
rehellinen <rehellinen@foxmail.com>
2020-02-13 13:37:02 +0800

    refactor: update @middleware to adapt to the new routerMap

--- src/decorator/decorator.js
+++ src/decorator/decorator.js
@@ -1,16 +1,24 @@
-/**
- *  validate.js
- *  Create By rehellinen
- *  Create On 2018/10/12 21:24
- */
-import { getMapKey, routerMap } from './router'
+import { routerMap } from '../router'
 
+/**
+ * 增加一个中间件到路由中
+ * @param middleware 中间件函数
+ * @returns {Function}
+ */
 export const middleware = (middleware) => {
   return (target, key, descriptor) => {
-    const mapKey = getMapKey(target, key, descriptor)
-    routerMap
-      .get(mapKey)
-      .actions
-      .unshift(middleware)
+    const mapKey = target.constructor.name + '.' + key
+    let routerConf = routerMap.get(mapKey)
+    if (!routerConf) {
+      routerConf = {}
+      routerMap.set(mapKey, routerConf)
+    }
+
+    let action = routerConf.action
+    action
+      ? Array.isArray(action)
+        ? action.unshift(middleware)
+        : action = [action, middleware]
+      : routerConf.action = [middleware]
   }
 }
