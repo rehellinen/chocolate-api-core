ff0a6bd3ceb7ab7944a7cec08e07452bff3f327a
rehellinen <rehellinen@foxmail.com>
2020-02-14 13:54:00 +0800

    fix: use the corret map to add prefix

--- src/router/prefix.js
+++ src/router/prefix.js
@@ -1,9 +1,9 @@
-import { normalizePath, routerMap } from './utils'
+import { normalizePath, orderedRouterMap } from './utils'
 
 export const prefix = (prefixStr, routerKeys) => {
   routerKeys = Array.prototype.concat.apply([], routerKeys)
   routerKeys.forEach(key => {
-    const config = routerMap.get(key)
+    const config = orderedRouterMap.get(key)
     config && (config.url = normalizePath(prefixStr) + config.url)
   })
 }
