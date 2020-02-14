ddee6e4bb441b44b9adb0f8c7d1f1d882cf7b687
rehellinen <rehellinen@foxmail.com>
2020-02-14 11:59:54 +0800

    refactor: use new api to write the routes

--- example/app/router.js
+++ example/app/router.js
@@ -1,8 +1,20 @@
-const { get, post, del, put } = require('libs')
+const { get, post, put, prefix, rest } = require('libs')
+
+prefix('user', [
+  get({
+    self: 'user.userGet' // 用户获取信息
+  }),
+  post({
+    login: 'user.login', // 登录
+    refresh: 'user.refresh' // 刷新access token
+  }),
+  put({
+    '/self/info': 'user.userUpdate', // 用户修改信息
+    '/self/pwd': 'user.userPassword', // 用户修改密码
+    '/self/avatar': 'user.avatar', // 用户修改头像
+    '/:id/password': 'user.password' // 管理员修改用户密码
+  }),
+  rest('user') // rest路由
+])
 
 
-post({
-  login: 'user.login',
-  refresh: 'user.refresh'
-})
-get('all', 'user.getAll')
