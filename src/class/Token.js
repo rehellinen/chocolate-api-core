44078ea7682adb80d5f52cd2ff69b19d05a4e96b
rehellinen <rehellinen@foxmail.com>
2020-02-20 11:39:28 +0800

    feat: add utils

--- src/class/Token.js
+++ src/class/Token.js
@@ -90,3 +90,56 @@ export class Token {
     return decoded
   }
 }
+
+/**
+ * 获取access_token
+ * @param id 用户的id
+ * @param payload 其他需要保存在JWT的数据
+ */
+export const getAccessToken = (id, payload) => Token.getInstance().getAccessToken(id, payload)
+
+/**
+ * 获取refresh_token
+ * @param id 用户的id
+ * @param payload 其他需要保存在JWT的数据
+ */
+export const getRefreshToken = (id, payload) => Token.getInstance().getRefreshToken(id, payload)
+
+/**
+ * 获取access_token和refresh_token
+ * @param id 用户的id
+ * @param payload 其他需要保存在JWT的数据
+ */
+export const getTokens = (id, payload) => {
+  const accessToken = Token.getInstance().getAccessToken(id, payload)
+  const refreshToken = Token.getInstance().getRefreshToken(id, payload)
+  return [accessToken, refreshToken]
+}
+
+/**
+ * 校验Token是否过期
+ * @param token refresh_token或access_token
+ */
+export const verifyToken = (token) => Token.getInstance().verify(token)
+
+/**
+ * 校验Token是否过期
+ * @param token access_token
+ */
+export const verifyAccessToken = (token) => {
+  const decoded = Token.getInstance().verify(token)
+  if (decoded.type !== TokenType.ACCESS) {
+    throw new InvalidToken({ message: '令牌类型错误' })
+  }
+}
+
+/**
+ * 校验Token是否过期
+ * @param token refresh_token
+ */
+export const verifyRefreshToken = (token) => {
+  const decoded = Token.getInstance().verify(token)
+  if (decoded.type !== TokenType.REFRESH) {
+    throw new InvalidToken({ message: '令牌类型错误' })
+  }
+}
