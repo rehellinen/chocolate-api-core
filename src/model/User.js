333742650bb92ae52783fff65031691f642b0888
rehellinen <rehellinen@foxmail.com>
2020-02-22 19:33:37 +0800

    feat: perfect user model

--- src/model/User.js
+++ src/model/User.js
@@ -1,22 +1,42 @@
-import { Sequelize, Model } from 'sequelize'
-import { NotFound, RepeatException } from '../exception'
-import { generatePwd } from '../utils'
+import { Sequelize } from 'sequelize'
+import { NotFound, RepeatException, NoAuthority } from '../exception'
+import { generatePwd, verifyPwd } from '../utils'
+import { RoleModel } from './Role'
+import { getTokens } from '../class'
+import { BaseModel } from './BaseModel'
 
-export class UserModel extends Model {
-  static async getAllUsers () {
-    const users = await this.findAll()
-    if (users.length === 0) {
+// TODO: Bug：软删除一个用户账户，再次添加会报错！
+export class UserModel extends BaseModel {
+  static async login (params) {
+    const user = await this.getUserByAccount(params.account)
+    if (verifyPwd(params.password, user.password)) {
+      return getTokens(user.id)
+    } else {
+      throw new NoAuthority({ message: '密码错误' })
+    }
+  }
+
+  static async getAllUsers (page, pageSize) {
+    const data = await this.pagination({
+      page,
+      pageSize,
+      options: {
+        include: [{
+          model: RoleModel
+        }]
+      }
+    })
+    if (data.data.length === 0) {
       throw new NotFound({
         message: '用户数据不存在'
       })
     }
-    return users
+    return data
   }
 
-  async getUserByAccount (account) {
-    const user = await this.getOne({
-      condition: { account },
-      relation: ['group']
+  static async getUserByAccount (account) {
+    const user = await this.findOne({
+      where: { account }
     })
     if (!user) {
       throw new NotFound({ message: '该账号不存在' })
@@ -24,10 +44,9 @@ export class UserModel extends Model {
     return user
   }
 
-  async getUserById (id) {
-    const user = await this.getOne({
-      condition: { id },
-      relation: ['group']
+  static async getUserById (id) {
+    const user = await this.findOne({
+      where: { id }
     })
     if (!user) {
       throw new NotFound({ message: '该用户不存在' })
@@ -35,21 +54,40 @@ export class UserModel extends Model {
     return user
   }
 
-  async createUser (data) {
-    data.password = generatePwd(data.password)
-    const user = await this.getOne({
-      condition: {
-        account: data.account
-      }
-    })
-    if (user) {
-      throw new RepeatException({ message: '该账号已存在' })
-    }
+  static async createUser (data) {
+    await this.isAccountExisted(data.account)
     await this.create(data)
   }
 
-  async updateUser (data) {
-    await this.updateById(data.id, data)
+  static async updateUser (data) {
+    await this.isAccountExisted(data.account, data.id)
+    await this.update(data, {
+      where: { id: data.id }
+    })
+  }
+
+  static async isAccountExisted (account, id) {
+    const user = await this.findOne({
+      where: {
+        account: account
+      }
+    })
+    if (id) {
+      if (user && user.id !== id) {
+        throw new RepeatException({ message: '该账号已存在' })
+      }
+    } else {
+      if (user) {
+        throw new RepeatException({ message: '该账号已存在' })
+      }
+    }
+  }
+
+  static async deleteUser (id) {
+    await this.getUserById(id)
+    await this.destroy({
+      where: { id }
+    })
   }
 
   async updatePassword (data) {
@@ -75,8 +113,11 @@ export const initUserModel = (db) => {
       unique: true
     },
     password: {
-      type: Sequelize.STRING(50),
-      allowNull: false
+      type: Sequelize.STRING(100),
+      allowNull: false,
+      set (val) {
+        this.setDataValue('password', generatePwd(val))
+      }
     },
     avatar: {
       type: Sequelize.STRING,
@@ -86,10 +127,17 @@ export const initUserModel = (db) => {
       type: Sequelize.INTEGER,
       allowNull: false,
       defaultValue: 1
+    },
+    order: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0
     }
   }, {
     sequelize: db,
     modelName: 'user',
     tableName: 'cms_user'
   })
+
+  UserModel.belongsTo(RoleModel)
 }
