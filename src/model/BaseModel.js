1b42ae50bd95a019f190e0db7a0f9c87008c41ef
rehellinen <rehellinen@foxmail.com>
2020-02-23 21:06:20 +0800

    improvement: add default value to options

--- src/model/BaseModel.js
+++ src/model/BaseModel.js
@@ -2,7 +2,7 @@ import { Model } from 'sequelize'
 import { config } from '../class'
 
 export class BaseModel extends Model {
-  static async pagination ({ page = 1, pageSize, options }) {
+  static async pagination ({ page = 1, pageSize, options = {} }) {
     pageSize = pageSize || config.get('model.page_size')
 
     options.limit = pageSize
