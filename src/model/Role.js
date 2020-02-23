7c94c6fffebb33479d0b8c7d46cf7b7fdefe918b
rehellinen <rehellinen@foxmail.com>
2020-02-23 21:05:16 +0800

    feat: add methods

--- src/model/Role.js
+++ src/model/Role.js
@@ -1,6 +1,47 @@
-import { Sequelize, Model } from 'sequelize'
+import { Sequelize } from 'sequelize'
+import { NotFound } from '../exception'
+import { BaseModel } from './BaseModel'
 
-export class RoleModel extends Model {
+export class RoleModel extends BaseModel {
+  static async getAllRoles (page, pageSize) {
+    const data = await this.pagination({
+      page,
+      pageSize
+    })
+    if (data.data.length === 0) {
+      throw new NotFound({
+        message: '分组数据不存在'
+      })
+    }
+    return data
+  }
+
+  static async getRoleById (id) {
+    const role = await this.findOne({
+      where: { id }
+    })
+    if (!role) {
+      throw new NotFound({ message: '该分组不存在' })
+    }
+    return role
+  }
+
+  static async createRole (data) {
+    await this.create(data)
+  }
+
+  static async updateRole (data) {
+    await this.update(data, {
+      where: { id: data.id }
+    })
+  }
+
+  static async deleteRole (id) {
+    await this.getRoleById(id)
+    await this.destroy({
+      where: { id }
+    })
+  }
 }
 
 export const initRoleModel = (db) => {
@@ -16,6 +57,7 @@ export const initRoleModel = (db) => {
     },
     desc: {
       type: Sequelize.STRING(50),
+      unique: true,
       allowNull: false
     },
     order: {
